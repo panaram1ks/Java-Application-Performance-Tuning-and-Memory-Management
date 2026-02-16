a81c5c8a6cb6914e381335e7ca3c09dc39dd4635
parom <E.Parominskiy@isida.by>
2026-02-16 17:35:21 +0300

    Section 2: Chapter 2 - Just In Time Compilation and the Code Cache

--- Starting Workspaces/java8/Chapter 02/PerformanceExample1/src/Main.java
+++ Starting Workspaces/java8/Chapter 02/PerformanceExample1/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.Date;
 
 public class Main {
 
-	public static void main(String[] args) {
-		PrimeNumbers primeNumbers = new PrimeNumbers();
-		Integer max = Integer.parseInt(args[0]);
-		primeNumbers.generateNumbers(max);
-	}
+    public static void main(String[] args) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        Integer max = Integer.parseInt(args[0]);
+        primeNumbers.generateNumbers(max);
+    }
 
 }
