aadcf867fbde96cc411e74db585a5fe90a12a22c
parom <E.Parominskiy@isida.by>
2026-02-17 12:19:46 +0300

    Section 7: Chapter 7 - Escaping References Strategy 1 - using an iterator

--- Starting Workspaces/java8/Chapter 07/EscapingReferences/src/Main.java
+++ Starting Workspaces/java8/Chapter 07/EscapingReferences/src/Main.java
@@ -1,16 +1,31 @@
+import java.util.Iterator;
+
 public class Main {
 
-	public static void main(String[] args) {
-		CustomerRecords records = new CustomerRecords();
+    public static void main(String[] args) {
+        CustomerRecords records = new CustomerRecords();
 
-		records.addCustomer(new Customer("John"));
-		records.addCustomer(new Customer("Simon"));
-				
-		for (Customer next : records.getCustomers().values())
-				{
-					System.out.println(next);
-				}
+        records.addCustomer(new Customer("John"));
+        records.addCustomer(new Customer("Simon"));
 
-	}
-	
+//		records.getCustomers().clear();
+//        for (Customer next : records.getCustomers().values()) {
+//            System.out.println(next);
+//        }
+
+		for (Customer record : records) {
+			System.out.println(record);
+		}
+
+        Iterator<Customer> it = records.iterator();
+        it.next();
+        it.remove();
+
+        for (Customer record : records) {
+            System.out.println(record);
+        }
+
+
+    }
+
 }
