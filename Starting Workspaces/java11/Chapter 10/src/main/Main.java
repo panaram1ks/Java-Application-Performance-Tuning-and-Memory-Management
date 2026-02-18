0f2f15f118729310c256ebfcd78ffdf2b8c70a55
parom <E.Parominskiy@isida.by>
2026-02-18 09:56:43 +0300

    Section 10: Chapter 10 - Tuning the JVM's Memory Settings 44. Tuning the size of the string pool

---
+++ Starting Workspaces/java11/Chapter 10/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date start = new Date();
+        List<String> strings = new ArrayList<>();
+        for (Integer i = 1; i < 1_000_000; i++) {
+            String s = i.toString().intern();
+            strings.add(s);
+        }
+        Date end = new Date();
+        System.out.println("Elapsed time was " + (end.getTime() - start.getTime()) + " ms");
+    }
+}
